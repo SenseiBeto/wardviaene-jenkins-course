d9c05dab90271bb67d600473dc7833df252ed00b
SenseiBeto <54226839+SenseiBeto@users.noreply.github.com>
2020-06-15 20:09:55 -0300

    nattyroots x r

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('nattyroots/senseibeto/docker-nodejs-demo')
+            repositoryName('r/senseibeto/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
